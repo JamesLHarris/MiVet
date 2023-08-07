a1fb6b67d75208a471170d08631aae22c0c3d734
JamesLHarris <112925051+JamesLHarris@users.noreply.github.com>
2023-08-06 22:25:23 -0700

    Add files via upload

---
+++ Front End/SingleSurvey.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import { Card, Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import surveyAnswersService from "services/surveyAnswersService";
+import Question from "./Question";
+import PropTypes from "prop-types";
+import toastr from "toastr";
+import debug from "sabio-debug";
+import { useLocation } from "react-router-dom";
+import { Form, Formik } from "formik";
+import { useMemo } from "react";
+import "./surveyanswer.css";
+import { useEffect } from "react";
+const _logger3 = debug.extend("ArrayLogger");
+
+function SingleSurvey() {
+  const state = useLocation();
+  const navigate = useNavigate();
+  const [instanceData, setInstanceData] = useState({
+    instanceId: 0,
+  });
+
+  const parentValue = {};
+
+  const survey = state.state?.payload.singleSurvey;
+
+  useEffect(() => {
+    let payload = { surveyId: survey.id };
+    surveyAnswersService
+      .createInstance(payload)
+      .then(createInstanceSuccess)
+      .catch(createInstanceError);
+  }, []);
+
+  const createInstanceError = () => {
+    toastr.error("Failed to create Instance.", "Error");
+  };
+
+  const createInstanceSuccess = (response) => {
+    _logger3(
+      survey,
+      "survey",
+      parentValue,
+      "answerState",
+      instanceData,
+      "intanceData",
+      response,
+      "instanceResponse"
+    );
+    let getData = response.item;
+    setInstanceData((prevState) => {
+      const pd = { ...prevState };
+      pd.instanceId = getData;
+      return pd;
+    });
+  };
+
+  const mapQuestions = (question) => (
+    <Question
+      question={question}
+      surveyId={survey.id}
+      key={question.id}
+      answerState={parentValue}
+      surveyInstance={instanceData.instanceId}
+    ></Question>
+  );
+
+  const questionComponents = useMemo(
+    () => survey?.questions?.map(mapQuestions),
+    [survey?.questions, instanceData.instanceId]
+  );
+
+  const goBack = () => {
+    navigate(`/dashboard/answers/available`);
+  };
+
+  const goSubmit = () => {
+    onSubmitSuccess();
+    navigate(`/dashboard/answers/available`);
+  };
+
+  const onSubmitSuccess = () => {
+    toastr.success("Thank You!");
+  };
+
+  return (
+    <Container className="d-flex justify-content-center">
+      <Card className="d-flex m-1 w-75 justify-content-center">
+        <Card.Header className="d-flex justify-content-center">
+          <h1>{survey.surveyName}</h1>
+        </Card.Header>
+        <Card.Body className="survey-question-container">
+          <Formik
+            enableReinitialize={true}
+            initialValues={parentValue}
+          >
+            <Form>
+              {questionComponents}
+              <Card.Footer>
+                <div className="d-flex survey-button-container">
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="survey-submit-button"
+                    onClick={goSubmit}
+                  >
+                    Submit
+                  </Button>
+                  <Button
+                    variant="primary"
+                    type="button"
+                    className="survey-go-back-button"
+                    onClick={goBack}
+                  >
+                    Cancel
+                  </Button>
+                </div>
+              </Card.Footer>
+            </Form>
+          </Formik>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
+
+SingleSurvey.propTypes = {
+  survey: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    surveyName: PropTypes.string.isRequired,
+    questions: PropTypes.arrayOf(
+      PropTypes.shape({
+        surveyName: PropTypes.string.isRequired,
+        question: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        questionTypeId: PropTypes.number.isRequired,
+        answerOptions: PropTypes.arrayOf(
+          PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            questionId: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+          })
+        ),
+      })
+    ),
+  }),
+  currentUser: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }),
+};
+
+export default SingleSurvey;
---
+++ Front End/SurveyAnswers.jsx
@@ -0,0 +1,126 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { Card, Col, Container, Row, Table, Breadcrumb } from "react-bootstrap";
+import PropTypes from "prop-types";
+import toastr from "toastr";
+import "../../toastr/build/toastr.css";
+import surveyAnswersService from "services/surveyAnswersService";
+import AvailableSurvey from "./AvailableSurvey";
+import debug from "sabio-debug";
+const _logger = debug.extend("SurveyAnswers");
+
+const SurveyAnswers = ({ currentUser }) => {
+  const [pageData] = useState({
+    pageIndex: 0,
+    pageSize: 30,
+    userId: currentUser.id,
+  });
+
+  const [surveys, setSurveys] = useState({
+    arrayOfSurveys: [],
+    surveyComponents: [],
+  });
+
+  _logger(surveys);
+
+  useEffect(() => {
+    surveyAnswersService
+      .getAllSurveysDetailedPaginated(pageData.pageIndex, pageData.pageSize)
+      .then(onGetUserSurveySuccess)
+      .catch(onGetError);
+  }, []);
+
+  const onGetUserSurveySuccess = (response) => {
+    let getData = response.item.pagedItems;
+    setSurveys((prevState) => {
+      const pd = { ...prevState };
+      let filteredData = getData.filter((survey) => survey.statusId === 1);
+      pd.surveyComponents = filteredData.map(mapSurveys);
+      pd.arrayOfSurveys = getData;
+      return pd;
+    });
+  };
+  const onGetError = () => {
+    toastr.error("Failed to load surveys on AnswersPage.", "Error");
+  };
+
+  const mapSurveys = (survey) => {
+    return (
+      <AvailableSurvey
+        singleSurvey={survey}
+        key={survey.id}
+        name={survey.surveyName}
+        description={survey.surveyDescription}
+        authors={survey.createdBy}
+        available={survey.dateModified}
+      ></AvailableSurvey>
+    );
+  };
+
+  return (
+    <Fragment>
+      <div className="py-4 py-lg-6 bg-colors-gradient d-lg-flex align-items-center justify-content-between">
+        <Container>
+          <Row>
+            <Col lg={{ span: 11, offset: 1 }} md={12} sm={12}>
+              <div className="mb-4 mb-lg-0">
+                <h1 className="text-black mb-1">Please Take Our Survey!</h1>
+                <p className="mb-0 text-black lead">
+                  We here at MiVet would love your input and how we can provide
+                  better service to you and our patients.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      <Card className="border-0">
+        <Card.Body>
+          <div>
+            <Row>
+              <Row>
+                <Col lg={12} md={12} sm={12}>
+                  <div className="border-bottom pb-4 mb-4 d-md-flex align-items-center justify-content-between">
+                    <div className="mb-3 mb-md-0">
+                      <Breadcrumb>
+                        <Breadcrumb.Item href="/dashboard/admin">
+                          Dashboard
+                        </Breadcrumb.Item>
+                        <Breadcrumb.Item active>
+                          Available Surveys
+                        </Breadcrumb.Item>
+                      </Breadcrumb>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Row>
+          </div>
+        </Card.Body>
+        {/*Table*/}
+        <Card.Body>
+          <Table responsive className="table">
+            <thead>
+              <tr>
+                <th scope="col">SURVEY</th>
+                <th scope="col">DESCRIPTION</th>
+                <th scope="col">AVAILABLE SINCE</th>
+                <th scope="col">BY</th>
+                <th scope="col">TAKE SURVEY</th>
+              </tr>
+            </thead>
+            <tbody>{surveys.surveyComponents}</tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </Fragment>
+  );
+};
+
+SurveyAnswers.propTypes = {
+  currentUser: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+  }),
+};
+
+export default SurveyAnswers;
