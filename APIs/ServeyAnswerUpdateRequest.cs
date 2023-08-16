0c87bdd20eda4ef2cbdcb2a7a7288322eb250672
JamesLHarris <112925051+JamesLHarris@users.noreply.github.com>
2023-08-16 09:46:04 -0700

    Update ServeyAnswerUpdateRequest.cs
    Cleaned

--- APIs/ServeyAnswerUpdateRequest.cs
+++ APIs/ServeyAnswerUpdateRequest.cs
@@ -1,11 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Requests.Surveys
 {
     public class ServeyAnswerUpdateRequest : SurveyAnswerAddRequest
     {
